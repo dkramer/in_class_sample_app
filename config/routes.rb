3d3668d89c4afd1a2d1ae93aeb8e8eaac52fcc4c
Dakota <kramerdakota@gmail.com>
2012-02-20 17:38:12 -0600

    finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 InClassSampleApp::Application.routes.draw do
-  get "pages/home"
+  get "users/new"
 
-  get "pages/contact"
-
-  get "pages/about"
-
-  get "pages/help"
-
+  match '/signup',  :to => 'users#new'
 
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
+  #match '/pages/home' => 'pages#home'
 
   root :to => 'pages#home'
   
