37a009b567ad790670ff6cbc5beaf792809f1c32
Dakota <kramerdakota@gmail.com>
2012-04-26 20:37:29 -0500

    before 11.13

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,10 +5,17 @@ Factory.define :user do |user|
   user.password_confirmation "foobar"
 end
 
+
+#TODO delete this sequence
 Factory.sequence :name do |n|
   "Person #{n}"
 end
 
 Factory.sequence :email do |n|
   "person-#{n}@example.com"
+end
+
+Factory.define :micropost do |micropost|
+  micropost.content "Foo bar"
+  micropost.association :user
 end
