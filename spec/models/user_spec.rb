4e1c055936c0716cc73ae56e33df2ae2bea3b8c9
Dakota <kramerdakota@gmail.com>
2012-05-03 17:50:42 -0500

    before 12.14

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -224,5 +224,27 @@ describe User do
     it "should have a relationships method" do
       @user.should respond_to(:relationships)
     end
+
+    it "should have a following method" do
+      @user.should respond_to(:following)
+    end
+  
+    it "should have a following? method" do
+      @user.should respond_to(:following?)
+    end
+
+    it "should have a follow! method" do
+      @user.should respond_to(:follow!)
+    end
+
+    it "should follow another user" do
+      @user.follow!(@followed)
+      @user.should be_following(@followed)
+    end
+
+    it "should include the followed user in the following array" do
+      @user.follow!(@followed)
+      @user.following.should include(@followed)
+    end
   end
 end
