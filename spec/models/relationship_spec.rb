7c10692579764794549634a8aa8fcea881bfb4b9
Dakota <kramerdakota@gmail.com>
2012-05-03 17:45:06 -0500

    before 12.13

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -12,4 +12,27 @@ describe Relationship do
   it "should create a new instance given valid attributes" do
     @relationship.save!
   end
+
+  describe "follow methods" do
+
+    before(:each) do
+      @relationship.save
+    end
+
+    it "should have a follower attribute" do
+      @relationship.should respond_to(:follower)
+    end
+
+    it "should have the right follower" do
+      @relationship.follower.should == @follower
+    end
+
+    it "should have a followed attribute" do
+      @relationship.should respond_to(:followed)
+    end
+
+    it "should have the right followed user" do
+      @relationship.followed.should == @followed
+    end
+  end
 end
