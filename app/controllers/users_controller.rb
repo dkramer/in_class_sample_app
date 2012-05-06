0f072156cff1b60ddf721c88827f0dc0e71b7b1d
Dakota <kramerdakota@gmail.com>
2012-05-06 08:43:36 -0500

    chapter 8 exercises

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,9 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       @title = "Sign up"
+      #@chapter8 exercise
+      @user.password = ''
+      @user.password_confirmation = ''
       render 'new'
     end
   end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -134,6 +134,28 @@ describe UsersController do
       get 'new'
       response.should have_selector("title", :content => "Sign up")
     end
+
+    # start #@chapter8 exercise
+    it "should have a name field" do
+      get :new
+      response.should have_selector("input[name='user[name]'][type='text']")
+    end
+
+    it "should have an email field" do
+      get :new
+      response.should have_selector("input[name='user[email]'][type='text']")
+    end
+
+    it "should have a password field" do
+      get :new
+      response.should have_selector("input[name='user[password]'][type='password']")
+    end
+
+    it "should have a password confirmation field" do
+      get :new
+      response.should have_selector("input[name='user[password_confirmation]'][type='password']")
+    end
+    # end #@chapter8 exercise
   end
 
 
